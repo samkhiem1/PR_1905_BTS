fd0b9f1ce6e02f53daa4acf4aa005400073e2c2f
samkhiem1 <samkhiem1@gmail.com>
2019-12-05 16:33:39 +0700

    admin check booking request

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :tour
-  validates :people_number, presence: true, numericality: {less_than: 12}
+  validates :people_number, presence: true, numericality: {less_than: 13}
   validates :phone, presence: true,length: { maximum: 10 }
   enum status: [:pending, :success, :cancel]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   root   'tours#index'
-  get  '/support',    to: 'static_pages#support'
-  get    '/help',     to: 'static_pages#help'
-  get    '/about',    to: 'static_pages#about'
-  get    '/contact',  to: 'static_pages#contact'
   get    '/signup',   to: 'users#new'
   get    '/login',    to: 'sessions#new'
   post   '/login',    to: 'sessions#create'
